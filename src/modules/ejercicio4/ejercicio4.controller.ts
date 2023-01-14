e9f723bce049a43731957c9192f2c628baa3e37b
Renatto Neciosup <renattonl@gmail.com>
2023-01-14 01:04:47 -0500

    finish exercise 4

--- src/modules/ejercicio4/ejercicio4.controller.ts
+++ src/modules/ejercicio4/ejercicio4.controller.ts
@@ -1,4 +1,5 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Controller, Get, Param, Res } from '@nestjs/common';
+import { Response } from 'express';
 import { Ejercicio4Service } from './ejercicio4.service';
 
 @Controller('ejercicio4')
@@ -6,7 +7,13 @@ export class Ejercicio4Controller {
   constructor(private readonly service: Ejercicio4Service) {}
 
   @Get('escenario1/:id_tribe')
-  escenario1(@Param('id_tribe') id_tribe: string) {
-    return this.service.escenario1(+id_tribe);
+  async escenario1(
+    @Param('id_tribe') id_tribe: string,
+    @Res() response: Response,
+  ) {
+    const csv = await this.service.escenario1(+id_tribe);
+    response.header('Content-Type', 'text/csv');
+    response.attachment('reporte.csv');
+    return response.send(csv);
   }
 }
--- src/modules/ejercicio4/ejercicio4.service.ts
+++ src/modules/ejercicio4/ejercicio4.service.ts
@@ -1,11 +1,14 @@
 import { Injectable } from '@nestjs/common';
 import { Ejercicio3Service } from '../ejercicio3/ejercicio3.service';
+import { Parser } from 'json2csv';
 
 @Injectable()
 export class Ejercicio4Service {
   constructor(private readonly serviceEjercicio3: Ejercicio3Service) {}
   async escenario1(id_tribe: number) {
-    const rows = await this.serviceEjercicio3.escenario3(id_tribe);
-    return rows;
+    const { repositories } = await this.serviceEjercicio3.escenario3(id_tribe);
+    const parser = new Parser();
+    const csv = parser.parse(repositories);
+    return csv;
   }
 }
